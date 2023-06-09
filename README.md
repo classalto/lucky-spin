3024e36d9b763e7379ce63a2c1692c2615add8ce
classalto <vbw293@gmail.com>
2023-06-08 22:07:17 -0700

    [Add] create deposit(), create getSelectedLines() - main.js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# lucky-spin
+# Lucky Spin
+
+###### created by: Victor Williams
+
+---
+
+##### Tooling
+
+> This project is built using Javascript and the NodeJS runtime. I also use the prompt-sync module for user inputs.
+
+##### How the web application works
+
+> Choose some amount of available virtual bucks to bet on any possible combination of the rows of the slot machine. Then startup the lucky slot machine and nervously wait for the results! Behind the scenes the program will determine if you won and will perform the neccessary calculations to give your expected payout. Lastly, start a new spin if you still have virtual bucks.
--- main.js
+++ main.js
@@ -1,7 +1,39 @@
+const prompt = require('prompt-sync')();
+
 // 1. Deposit Money
+const depositMoney = () => {
+  while (true) {
+    const depositAmount = prompt('Enter any amount of Virtual Bucks: ');
+    const convertedDeposit = parseFloat(depositAmount);
+
+    if (isNaN(convertedDeposit) || convertedDeposit <= 0) {
+      console.log('Invalid amount of virtual bucks... Please try again.');
+    } else {
+      return convertedDeposit;
+    }
+  }
+};
+
 // 2. Determine the number of lines to bet on
+const getSelectedLines = () => {
+  while (true) {
+    const lines = prompt(
+      'Enter the number of lines (1-3) you wish to bet on: '
+    );
+    const convertedLines = parseFloat(lines);
+
+    if (isNaN(convertedLines) || convertedLines < 1 || convertedLines > 3) {
+      console.log('Invalid lines chosen... Please try again.');
+    } else {
+      return convertedLines;
+    }
+  }
+};
 // 3. Collect the correct bet amount
 // 4. Spin the slot machine
 // 5. Check if the user won
 // 6. Adjust money balance
 // 7. Play again
+
+const wallet = depositMoney();
+const chosenLines = getSelectedLines();
