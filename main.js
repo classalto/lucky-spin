de919c3e06787447cc0302d27d198d94bd67efff
classalto <vbw293@gmail.com>
2023-06-08 23:57:24 -0700

    [ADD] create placeBet(), create Global variables - main.js

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+// imports
 const prompt = require('prompt-sync')();
+
+// GLOBAL
+const ROWS = 3;
+const COLUMNS = 3;
+const SYMBOL_COUNTS = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+const SYMBOLS_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
 // 1. Deposit Money
 const depositMoney = () => {
@@ -30,10 +47,31 @@ const getSelectedLines = () => {
   }
 };
 // 3. Collect the correct bet amount
+const placeBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt(
+      'Enter the amount of virtual bucks per line you would like to bet: '
+    );
+    const convertedBet = parseFloat(bet);
+
+    // Confirm there is enough money to cover the bet and then update balance
+    if (isNaN(convertedBet) || convertedBet <= 0) {
+      console.log('You need to bet a valid amount of virtual bucks!');
+    } else if (convertedBet > balance) {
+      console.log(
+        "You don't have enough virtual bucks... enter what you can afford."
+      );
+    } else {
+      return convertedBet;
+    }
+  }
+};
+
 // 4. Spin the slot machine
 // 5. Check if the user won
 // 6. Adjust money balance
 // 7. Play again
 
-const wallet = depositMoney();
-const chosenLines = getSelectedLines();
+let balance = depositMoney();
+const lines = getSelectedLines();
+const bet = placeBet(balance, lines);
